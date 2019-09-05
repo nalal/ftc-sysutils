f3c010d8ae04b106c4810588ecf90ff86de94842
nalal <noahgman11@gmail.com>
2019-09-05 14:31:02 -0600

    added argument handling for fcount

--- src/cpp/fcount.cpp
+++ src/cpp/fcount.cpp
@@ -4,16 +4,36 @@
 
 // C includes
 #include <stdint.h>
+#include <string.h>
+
+/*
+ *
+ */
 
 namespace fs = std::experimental::filesystem;
 
 int main(uint8_t argc, char* argv[])
 {
+	bool cleanOut = false;
+	for(uint8_t i = 1; i < argc; i++)
+	{
+		if(strcmp(argv[i], "-s") == 0)
+		{
+			cleanOut = true;
+		}
+	}
 	int files = 0;
 	for(auto& p: fs::directory_iterator("./"))
 	{
 		files++;
 	}
-	std::cout << "Total files: " << files << "\n";
+	if(cleanOut)
+	{
+		std::cout << files << "\n";
+	}
+	else
+	{
+		std::cout << "Total files: " << files << "\n";
+	}
 	return 0;
 }
