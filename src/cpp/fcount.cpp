0827473ac020290af686be1659d3ca6176af7236
nalal <noahgman11@gmail.com>
2019-09-05 14:34:42 -0600

    added targeting arg

--- src/cpp/fcount.cpp
+++ src/cpp/fcount.cpp
@@ -15,11 +15,16 @@ namespace fs = std::experimental::filesystem;
 int main(uint8_t argc, char* argv[])
 {
 	bool cleanOut = false;
+	char* dir;
 	for(uint8_t i = 1; i < argc; i++)
 	{
 		if(strcmp(argv[i], "-s") == 0)
 		{
 			cleanOut = true;
+		}
+		else if(strcmp(argv[i], "-t") == 0)
+		{
+			dir = argv[i+1];
 		}
 	}
 	int files = 0;
