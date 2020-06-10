59939e8a1c6f4914ff76b089b8205f4caa35bcd0
nalal <noahgman11@gmail.com>
2020-06-10 15:22:25 +0000

    added rname to sysutils

--- src/cpp/rname.cpp
+++ src/cpp/rname.cpp
@@ -1,46 +1,62 @@
-// C++ includes
-#include <iostream>
-#include <experimental/filesystem>
+#include "rname.h"
 
-// C includes
-#include <stdint.h>
-#include <string.h>
+namespace fs = std::experimental::filesystem;
+using std::string;
 
-/*
- *======================================================================
- *	NAME: 
+/*======================================================================
+ * NAME:
  * 		rname (ReName)
- * 
- * 	AUTHOR:
+ * AUTHOR:
  * 		Nac/Nalal/Noah
- * 
- * 	Usecase:
- * 		Intended to be a simple rename utility to rename files
+ * USECASE:
+ * 		Intended to let a user rename a file without using mv and 
+ * 	entering the directory to the file twice
  *======================================================================
  */
 
-namespace fs = std::experimental::filesystem;
 
-using std::string;
-using std::cout;
-
-int main(uint8_t argc, char* argv[])
+//Create the new name for the file
+//[path] = original path of file
+//[name] = name to change to
+string get_name(char * path, char * name)
 {
-	string file = "";
-	string name = "";
-	if(argc < 2)
+	string s_path = path;
+	string s_name = name;
+	//If the file is not in a subdirectory, return name
+	switch(s_path.find("/"))
 	{
-		cout << "Missing agruments.\n";
-		return 0;
+		case string::npos:
+			return s_name;
 	}
-	else
+	std::size_t found = s_path.find_last_of("/");
+	string new_path = s_path.substr(0,found) + s_name;
+	return new_path;
+}
+
+int main(int argc, char * argv[])
+{
+	//Verify that the correct arguments were provided
+	switch(argc)
 	{
-		file = argv[0];
-		name = argv[1];
+		case 1:
+			printf("File not provided\n");
+			return 0;
+		case 2:
+			printf("New name not provided\n");
+			return 0;
 	}
-	if(fs::exists(file))
+	//Verify that the file exists
+	switch(fs::exists(argv[1]))
 	{
-		
+		case false:
+			printf("File not found\n");
+			return 0;
 	}
+	string new_name = get_name(argv[1], argv[2]);
+	//printf("%s\n%s", argv[1], new_name.c_str());
+	char command[2550];
+	sprintf(command, "mv %s %s", argv[1], new_name.c_str());
+	printf("%s\n", command);
+	system(command);
 	return 0;
 }
---
+++ src/cpp/rname.h
@@ -0,0 +1,6 @@
+//C includes
+#include <stdio.h>
+
+//C++ includes
+#include <experimental/filesystem>
+#include <string>
