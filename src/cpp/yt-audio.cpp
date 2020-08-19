8ed96bf4048fa19f4fa7a33d8af170bfb1952337
nalal <noahgman11@gmail.com>
2020-08-19 06:43:38 +0000

    changed if to switch

--- src/cpp/yt-audio.cpp
+++ src/cpp/yt-audio.cpp
@@ -6,30 +6,30 @@ int main(int argc, char * argv[])
 	char * save_dir = "~/music";
 	printf("[YT-AUDIO]\n");
 	printf("=Checking for URLs...\n");
-	if(argc > 1)
+	switch(argc)
 	{
-		char dir_make[256];
-		printf("=Validating music DIR...\n");
-		sprintf(dir_make, "mkdir -p %s", save_dir);
-		system(dir_make);
-		sprintf(dir_make, NULL);
-		for(int i = 1; i < argc; i++)
-		{
-			printf("=Downloading %s\n", argv[i]);
-			char runline[256];
-			sprintf(
-				runline, 
-				"youtube-dl -f bestaudio %s -o '%s/\%(title)s.\%(ext)s'", 
-				argv[i], 
-				save_dir
-			);
-			system(runline);
-			sprintf(runline, NULL);
-		}
-	}
-	else
-	{
-		printf("=No URLs provided, halting.\n");
+		case 1:
+			printf("=No URLs provided, halting.\n");
+			break;
+		default:
+			char dir_make[256];
+			printf("=Validating music DIR...\n");
+			sprintf(dir_make, "mkdir -p %s", save_dir);
+			system(dir_make);
+			sprintf(dir_make, NULL);
+			for(int i = 1; i < argc; i++)
+			{
+				printf("=Downloading %s\n", argv[i]);
+				char runline[256];
+				sprintf(
+					runline, 
+					"youtube-dl -f bestaudio %s -o '%s/\%(title)s.\%(ext)s'", 
+					argv[i], 
+					save_dir
+				);
+				system(runline);
+				sprintf(runline, NULL);
+			}
 	}
 	//sprintf(save_dir, NULL);
 	return 0;
