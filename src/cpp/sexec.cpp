a88054fe9b0ecd44c4ba5bc0be69e6f8e21a100a
nalal <noahgman11@gmail.com>
2021-04-20 17:33:14 -0600

    added SEXEC and updated makefile install

---
+++ src/cpp/sexec.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+using std::string;
+
+int main(int argc, char * argv[])
+{
+	const char * envvar = "SEXEC_DIR";
+	string comm = "$SHELL ";
+	if(!getenv(envvar)){
+		fprintf(stderr, "The environment variable %s is not set\n", envvar);
+		exit(1);
+	}
+	const char * script_dir = getenv(envvar);
+	comm = comm + script_dir + "/";
+	for(int i = 1; i < argc; i++)
+	{
+		comm = comm + argv[i] + " ";
+	}
+	system(comm.c_str());
+	return 0;
+}
